e838cfcf8374fd9dface2ce3cbf83f211fe77cf4
maartinV2 <39085235+maartinV2@users.noreply.github.com>
2020-03-07 14:05:40 +0200

    maartin
    change log
    1-added form 4-(add GPU and CPU) 
    2- added form 5 - welcome page -we still need to optimize welcome page so it looks better 
    3-form 2- log in code and data string compiled and confirmed to be  working

--- DB Projek 1/DB Projek 1/Form2.Designer.cs
+++ DB Projek 1/DB Projek 1/Form2.Designer.cs
@@ -30,10 +30,9 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.tbxUsername = new System.Windows.Forms.TextBox();
+            this.tbxUserName = new System.Windows.Forms.TextBox();
             this.tbxPassword = new System.Windows.Forms.TextBox();
             this.btnLogin = new System.Windows.Forms.Button();
-            this.btnExit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -41,7 +40,7 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(69, 48);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(74, 16);
+            this.label1.Size = new System.Drawing.Size(77, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Username:";
             // 
@@ -50,16 +49,16 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(69, 106);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(71, 16);
+            this.label2.Size = new System.Drawing.Size(73, 17);
             this.label2.TabIndex = 1;
             this.label2.Text = "Password:";
             // 
-            // tbxUsername
+            // tbxUserName
             // 
-            this.tbxUsername.Location = new System.Drawing.Point(167, 45);
-            this.tbxUsername.Name = "tbxUsername";
-            this.tbxUsername.Size = new System.Drawing.Size(100, 22);
-            this.tbxUsername.TabIndex = 2;
+            this.tbxUserName.Location = new System.Drawing.Point(167, 45);
+            this.tbxUserName.Name = "tbxUserName";
+            this.tbxUserName.Size = new System.Drawing.Size(100, 22);
+            this.tbxUserName.TabIndex = 2;
             // 
             // tbxPassword
             // 
@@ -79,28 +78,19 @@
             this.btnLogin.UseVisualStyleBackColor = true;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
-            // btnExit
-            // 
-            this.btnExit.Location = new System.Drawing.Point(192, 179);
-            this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(75, 23);
-            this.btnExit.TabIndex = 5;
-            this.btnExit.Text = "EXIT";
-            this.btnExit.UseVisualStyleBackColor = true;
-            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(326, 248);
-            this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.tbxPassword);
-            this.Controls.Add(this.tbxUsername);
+            this.Controls.Add(this.tbxUserName);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form2";
             this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -110,9 +100,8 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox tbxUsername;
+        private System.Windows.Forms.TextBox tbxUserName;
         private System.Windows.Forms.TextBox tbxPassword;
         private System.Windows.Forms.Button btnLogin;
-        private System.Windows.Forms.Button btnExit;
     }
 }
