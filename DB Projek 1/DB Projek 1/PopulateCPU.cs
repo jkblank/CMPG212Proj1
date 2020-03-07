795dd0db32a3eb757429003333857e49554f7e45
jkblank <60937724+jkblank@users.noreply.github.com>
2020-03-07 14:29:09 +0200

    Populate CPU Updated

--- DB Projek 1/DB Projek 1/PopulateCPU.cs
+++ DB Projek 1/DB Projek 1/PopulateCPU.cs
@@ -1,64 +1,70 @@
+/*
+*   John Klerck
+*/
+
 using System;
+using System.Data.SqlClient;
 
 namespace DB_Projek_1
 {
     public class PopulateCPU
     {
         private string sqlpiece1, sqlpiece2, sqlpiece3, sqlpiece4, sqlpiece5="";
+        private string sqlStatement="SELECT * FROM CPU ";
         
 
         public void Populate()
         {
-            if(!cmbManu.SelectedIndex==-1)
+            if(!cbMan.SelectedIndex==-1)
             {
-                sqlpiece1="WHERE Manufacturer LIKE "+cmbManu.Items[cmbManu.SelectedIndex].ToString() + " AND ";
+                sqlpiece1="WHERE Manufacturer LIKE "+cbMan.Items[cbMan.SelectedIndex].ToString() + " AND ";
             }
 
-            if(!cmbModel.SelectedIndex==-1)
+            if(!cbModel.SelectedIndex==-1)
             {
-                sqlpiece2="WHERE Model LIKE "+cmbModel.Items[cmbModel.SelectedIndex].ToString()+ " AND ";
+                sqlpiece2="WHERE Model LIKE "+cbModel.Items[cbModel.SelectedIndex].ToString()+ " AND ";
             }
 
-            if(!cmbYear.SelectedIndex==-1)
+            if(!cbYear.SelectedIndex==-1)
             {
-                sqlpiece3="WHERE Year LIKE "+cmbYear.Items[cmbYear.SelectedIndex].ToString()+ " AND ";
+                sqlpiece3="WHERE YearReleased LIKE "+cbYear.Items[cbYear.SelectedIndex].ToString()+ " AND ";
                 //sqlpiece3= "WHERE year BETWEEN "
             }
 
-            if(!cmbPrice.SelectedIndex==-1)
+            if(!cbPrice.SelectedIndex==-1)             //Price SQLPiece 4
             {
-                if (cmbPrice.SelectedIndex==0)      //0-1k
+                if (cbPrice.SelectedIndex==0)      //0-1k
                 {
-                    sqlpiece4="WHERE Price BETWEEN ";
+                    sqlpiece4="WHERE Price BETWEEN 1 AND 999 AND ";
                 }
 
-                if (cmbPrice.SelectedIndex==1)      //1k-2k5
+                if (cbPrice.SelectedIndex==1)      //1k-2k5
                 {
-                    
+                    sqlpiece4="WHERE Price BETWEEN 1000 AND 2499 AND ";
                 }
 
-                if (cmbPrice.SelectedIndex==2)      //2k5-5k
+                if (cbPrice.SelectedIndex==2)      //2k5-5k
                 {
-                    
+                    sqlpiece4="WHERE Price BETWEEN 2500 AND 4999 AND ";
                 }
 
-                if (cmbPrice.SelectedIndex==3)      //5k-10k
+                if (cbPrice.SelectedIndex==3)      //5k-10k
                 {
-                    
+                    sqlpiece4="WHERE Price BETWEEN 5000 AND 9999 AND ";
                 }
 
-                if (cmbPrice.SelectedIndex==4)      //10k+
+                if (cbPrice.SelectedIndex==4)      //10k+
                 {
-                    
+                    sqlpiece4="WHERE Price BETWEEN 10000 AND 150000 AND ";
                 }
             }
 
-            if (!cmbCores.SelectedIndex==-1)
+            if (!cbCores.SelectedIndex==-1)
             {
-                sqlpiece5="WHERE CoreCount Like " +cmbCores.Items[cmbCores.SelectedIndex].ToString()+ " AND ";//+" AND ";
+                sqlpiece5="WHERE Cores Like " +cbCores.Items[cbCores.SelectedIndex].ToString()+ " AND ";//+" AND ";
             }
 
-            string sqlStatement="SELECT * FROM CPU ";
+            
 
             if(sqlpiece1!="")
             {
@@ -73,11 +79,70 @@ namespace DB_Projek_1
             }
             if(sqlpiece3!="")
             {
-                sqlStatement+=sqlpiece3; //MODEL
+                sqlStatement+=sqlpiece3; //year
+                
+            }
+            if(sqlpiece4!="")
+            {
+                sqlStatement+=sqlpiece3; //Price
                 
             }
 
             sqlStatement+= "InStock BETWEEN 1 AND 10000";
+
+            //Finally use the tiny string that 
+            //shitshow of code was used to make
+
+            try
+            {
+                conn.Open();
+                SqlCommand com;
+                adapt= new SqlDataAdapter();
+                ds = new DataSet();
+
+                com=new SqlCommand(sqlStatement, conn);
+
+                adapt.SelectCommand=com;
+                adapt.Fill(ds, "CPUs");
+
+                //populate datagridview
+
+                dataGirdView1.DataSource=ds;
+                dataGirdView1.DataMember="CPUs";
+
+                //Populate combo box
+                SqlDataReader datReader = com.ExecuteReader();
+                string output = "";
+                while (datReader.Read())
+                {
+                    if (!cbMan.Items.Contains(datReader.GetValue(4)))
+                    {
+
+                        //check these index values
+                        cbMan.Items.Add(datReader.GetValue(4));
+                        cbModel.Items.Add(datReader.GetValue(4));
+                        cbStock.Items.Add(datReader.GetValue(4));
+                        cbYear.Items.Add(datReader.GetValue(4));
+                        cbCores.Items.Add(datReader.GetValue(4));
+                        cbPrice.Items.Add(datReader.GetValue(4));
+                    }
+                }//While end
+                
+
+                conn.Close();
+
+            }
+            catch(SqlExeption error)
+            {
+                MessageBox.Show(error.Message);
+
+            }
+
+        }
+
+        public void main (string [] args)
+        {
+            Populate();
         }
     }
 }
